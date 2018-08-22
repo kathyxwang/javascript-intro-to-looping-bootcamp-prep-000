d8763ab37ddfdb9bc5f43a9df5aab1849002b0a8
Kathy Wang <kathy.x.wang@gmail.com>
2018-08-22 02:07:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,9 +23,11 @@ function incrementVariable() {
 
 function maybeTrue() {
   return Math.random() >= 0.5;
+}
 
 function doWhileLoop(array) {
   do {
-    
-  }
+    array.pop();
+  } while (array.length > 0 && maybeTrue())
+  return array;
 }
